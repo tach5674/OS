c09aea3f8bf577f7a6547a687053e89892238d21
tach5674 <63715658+tach5674@users.noreply.github.com>
2023-03-07 11:59:47 +0400

    Create init.cpp

---
+++ Homework2/init.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+int main() {
+  key_t key = ftok("shm_file", 1);
+  int shmid = shmget(key, 300 * sizeof(int), 0600 | IPC_CREAT | IPC_EXCL);
+  if (shmid == -1) {
+    if (errno != EEXIST) {
+      std::cout << "ERROR: Cannot create shared mem segment!!\n";
+      exit(1);
+    } else {
+      shmid = shmget(key, 0, 0600);
+    }
+  }
+  void *shmp = shmat(shmid, NULL, 0);
+  if (shmp == (void *)-1) {
+    perror("Shared memory attach");
+    return 1;
+  }
+  int *arr = (int *)shmp;
+  for (int i = 0; i < 300; ++i) {
+    arr[i] = 0;
+  }
+  if (shmdt(shmp) == -1) {
+    perror("shmdt");
+    return 1;
+  }
+  return 0;
+}
