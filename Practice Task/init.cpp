265b8bb75bf0046bbe7b52ddd4e750e63c608223
tach5674 <63715658+tach5674@users.noreply.github.com>
2023-05-05 17:47:00 +0400

    Update init.cpp

--- Practice Task/init.cpp
+++ Practice Task/init.cpp
@@ -1,13 +1,14 @@
+#include <fcntl.h>
 #include <iostream>
 #include <sys/ipc.h>
 #include <sys/shm.h>
-#include <fcntl.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[]) {
   int n = atoi(argv[1]);
   creat("shm_file", 0600);
   key_t key = ftok("shm_file", 1);
-  int shmid = shmget(key, (n * 4 + 1) * sizeof(int), 0600 | IPC_CREAT | IPC_EXCL);
+  int shmid =
+      shmget(key, (n * 4 + 1) * sizeof(int), 0600 | IPC_CREAT | IPC_EXCL);
   if (shmid == -1) {
     if (errno != EEXIST) {
       std::cout << "ERROR: Cannot create shared mem segment!!\n";
@@ -24,17 +25,16 @@ int main(int argc, char* argv[]){
   }
 
   int *bank = (int *)shmp;
-  bank[0] = n * 4;
-  for(int i = 1; i < n * 4; ++i){
-    if(i % 4 == 3){
+  bank[0] = n;
+  for (int i = 1; i <= n * 4; ++i) {
+    if (i % 4 == 3) {
       bank[i] = 100;
       continue;
     }
     bank[i] = 0;
   }
   if (shmdt(shmp) == -1) {
-      perror("shmdt");
-      exit(1);
+    perror("shmdt");
+    exit(1);
   }
 }
-
