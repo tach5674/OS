56df59b025ef32697490a83f7617c213014b41b1
tach5674 <63715658+tach5674@users.noreply.github.com>
2023-05-18 14:42:58 +0400

    Update init.cpp

--- Practice Task/init.cpp
+++ Practice Task/init.cpp
@@ -1,40 +1,37 @@
 #include <fcntl.h>
-#include <iostream>
-#include <sys/ipc.h>
-#include <sys/shm.h>
+#include "header.cpp"
 
 int main(int argc, char *argv[]) {
   int n = atoi(argv[1]);
   creat("shm_file", 0600);
   key_t key = ftok("shm_file", 1);
-  int shmid =
-      shmget(key, (n * 4 + 1) * sizeof(int), 0600 | IPC_CREAT | IPC_EXCL);
+  int shmid = shmget(key, n * sizeof(BankAccount), 0600 | IPC_CREAT | IPC_EXCL);
   if (shmid == -1) {
     if (errno != EEXIST) {
-      std::cout << "ERROR: Cannot create shared mem segment!!\n";
+      std::cout << "ERROR: Cannot create shared mem segment!\n";
       exit(1);
     } else {
       shmid = shmget(key, 0, 0600);
     }
   }
 
-  void *shmp = shmat(shmid, NULL, 0);
-  if (shmp == (void *)-1) {
+  BankAccount* bank = (BankAccount*)shmat(shmid, NULL, 0);
+  if (bank == (BankAccount*)-1) {
     perror("Shared memory attach");
     exit(1);
   }
 
-  int *bank = (int *)shmp;
-  bank[0] = n;
-  for (int i = 1; i <= n * 4; ++i) {
-    if (i % 4 == 3) {
-      bank[i] = 100;
-      continue;
-    }
-    bank[i] = 0;
+  for (int i = 0; i < n; ++i) {
+      bank[i].current_balance = 0;
+      bank[i].minimal_balance = 0;
+      bank[i].maximal_balance = 10000;
+      bank[i].is_frozen = false;
   }
-  if (shmdt(shmp) == -1) {
+  
+  if (shmdt(bank) == -1) {
     perror("shmdt");
     exit(1);
   }
+
+  Semaphore sem("sem_file");
 }
